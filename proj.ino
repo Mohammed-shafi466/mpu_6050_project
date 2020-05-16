d63106fa82e211e2dd052d4edc49e1b11be5c3fc
Mohammed-shafi466 <61986326+Mohammed-shafi466@users.noreply.github.com>
2020-05-16 23:13:34 +0530

    updated project with leds
    made leds to glow when mpu accelarometer reading changes in x,y,z directions

--- proj.ino
+++ proj.ino
@@ -2,10 +2,11 @@
 
 const int MPU6050_addr=0x68;
 
-int16_t AccX,AccY,AccZ,Temp,GyroX,GyroY,GyroZ;
-
+int16_t AccX,AccY,AccZ;
+int AX=0,AY=0,AZ=0;
+int p8=8,p9=9,p10=10;
 void setup(){
-
+  
   Wire.begin();
 
   Wire.beginTransmission(MPU6050_addr);
@@ -17,46 +18,48 @@ void setup(){
   Wire.endTransmission(true);
 
   Serial.begin(9600);
+  pinMode(p8,OUTPUT);
+  pinMode(p9,OUTPUT);
+  pinMode(p10,OUTPUT);
 
 }
 
 void loop(){
 
   Wire.beginTransmission(MPU6050_addr);
-
   Wire.write(0x3B);
-
   Wire.endTransmission(false);
-
-  Wire.requestFrom(MPU6050_addr,14,true);
-
+  Wire.requestFrom(MPU6050_addr,6,true);
   AccX=Wire.read()<<8|Wire.read();
+  if (AX != AccX/16384)
+  {
+    digitalWrite(p8,HIGH);
+    }
+    else{
+      digitalWrite(p8,LOW);
+      }
+  AX=AccX/16384;
   
   AccY=Wire.read()<<8|Wire.read();
-
+    if (AY != AccY/16384)
+  {
+    digitalWrite(p9,HIGH);
+    }
+    else{
+      digitalWrite(p9,LOW);
+      }
+  AY=AccY/16384;
   AccZ=Wire.read()<<8|Wire.read();
-
-  Temp=Wire.read()<<8|Wire.read();
-
-  GyroX=Wire.read()<<8|Wire.read();
-
-  GyroY=Wire.read()<<8|Wire.read();
-
-  GyroZ=Wire.read()<<8|Wire.read();
-
-  Serial.print("AccX = "); Serial.print(AccX);
-  
-
-  Serial.print(" || AccY = "); Serial.print(AccY);
-
-  Serial.print(" || AccZ = "); Serial.print(AccZ);
-
-  Serial.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
-
-  Serial.print(" || GyroX = "); Serial.print(GyroX);
-
-  Serial.print(" || GyroY = "); Serial.print(GyroY);
-  Serial.print(" || GyroZ = "); Serial.println(GyroZ);
-  
+    if (AZ != AccZ/16384)
+  {
+    digitalWrite(p10,HIGH);
+    }
+    else{
+      digitalWrite(p10,LOW);
+      }
+  AZ=AccZ/16384;
+  Serial.print(" || AccX = "); Serial.print(AX);
+  Serial.print(" || AccY = "); Serial.print(AY);
+  Serial.print(" || AccZ = "); Serial.println(AZ);
   delay(100);
 }
